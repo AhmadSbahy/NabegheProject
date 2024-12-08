2f00fc1bbe0a020be81b4060d939a159fef8f65f
AhmadSbahy <167585826+AhmadSbahy@users.noreply.github.com>
2024-12-09 02:05:16 +0400

    Add Response Category To Index Page

---
+++ Nabeghe.Web/Components/SiteHeaderResponsiveViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Nabeghe.Infra.Data.Context;
+
+namespace Nabeghe.Web.Components
+{
+	public class SiteHeaderResponsiveViewComponent(NabegheContext context) : ViewComponent
+	{
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var categories = await context.CourseCategories
+				.Where(c => !c.IsDeleted)
+				.ToListAsync();
+
+			return View("/Views/Shared/Components/SiteHeaderResponsive.cshtml", categories);
+		}
+	}
+}
